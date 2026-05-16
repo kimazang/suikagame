72e7f493231f79ff78b0e55560b60b15ad1947dd
kimazang <kimazang@gmail.com>
2026-05-16 22:31:48 +0900

    Update game.js

--- game.js
+++ game.js
@@ -489,8 +489,8 @@ function toGameX(clientX) {
 }
 
 function renderFrame() {
-  // 배경
-  ctx.fillStyle = '#e8f5ff';
+  // 배경 (박스 안쪽 색과 동일)
+  ctx.fillStyle = '#cceeff';
   ctx.fillRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
 
   // DANGER 경고선
